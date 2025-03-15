627dbf71c053448e9ec4459696e9e7ffe93f6093
twwhite84 <twwhite.pvt@gmail.com>
2025-03-15 23:47:43 +1300

    removed SoftwareRenderer class, dont need it

--- SoftwareRenderer.py
+++
@@ -1,9 +0,0 @@
-import sdl2
-
-class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
-    def __init__(self, window):
-        super(SoftwareRenderer, self).__init__(window)
-
-    def render(self, components):
-        sdl2.ext.fill(self.surface, sdl2.ext.Color(0,0,0))
-        super(SoftwareRenderer, self).render(components)
